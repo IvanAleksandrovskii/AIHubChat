b3f709435cd041f5352f3c1c9365503b98803698
Ivan Aleksandrovskii <ivan.vered@gmail.com>
2025-03-18 13:50:41 +0700

    Dockerfile and configs front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ['8635-184-22-34-53.ngrok-free.app'],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: [env.VITE_APP_HOST],
+    },
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
+    },
+    build: {
+      rollupOptions: {
+        external: [],
+      }
+    }
+  }
 })
